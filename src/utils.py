f58128fcd1b0f2f19bd81fdb47bc2a85d1a91716
ankitghai83 <ankit.ghai@gmail.com>
2024-03-24 20:36:27 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ from src.logger import logging
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -24,12 +25,21 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 # Create model_evaluate function that will be called in model trainer
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         for i in range(len(models)):
             model=list(models.values())[i]
-            model.fit(X_train,y_train) # train model
+
+            para=param[list(models.keys())[i]] # Hyper parameter tuning
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train,y_train) # train model
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
 
